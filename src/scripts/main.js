c527d8d3fbb97abf3d566e100bde8ab77ef5c53b
mattkelsey <mattkelseymk@gmail.com>
2016-10-15 16:05:59 -0700

    Add timer slide and useful classes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,7 +10,7 @@ var bespoke = require('bespoke'),
   hash = require('bespoke-hash');
 
 // Bespoke.js
-bespoke.from('article', [
+var deck = bespoke.from('article', [
   nebula(),
   keys(),
   touch(),
@@ -25,4 +25,48 @@ bespoke.from('article', [
 // This is actually loaded from "bower_components" thanks to
 // debowerify: https://github.com/eugeneware/debowerify
 require('prism');
+
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+function initializeClock(endtime) {
+  var minutesSpan = document.querySelector('.minutes');
+  var secondsSpan = document.querySelector('.seconds');
+
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+
+    minutesSpan.innerHTML = (t.minutes);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
+
+var activated = false
+
+deck.on('activate', function(event){
+    if(event.slide.getAttribute('class').includes('clockSlide') && !activated) {
+        var deadline = new Date(Date.parse(new Date()) + 5 * 60 * 1000); //five minutes from now
+        initializeClock(deadline);
+        activated = true;
+    }
+})
 
